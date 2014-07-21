575b34dbd26b09a39967d5652af303f878e4fef6
Marcus Tang <tmc9031@gmail.com>
2014-07-21 14:10:39 +0800

    new: support LOCAL_OBJECT_FLAGS [special]

--- Modules.mk
+++ Modules.mk
@@ -9,5 +9,10 @@ LOCAL_SRC_FILES :=
 LOCAL_SRC_FILES += main.c
 LOCAL_SRC_FILES += func.c
 
+LOCAL_CFLAGS := -DLOCAL
+
+LOCAL_OBJECT_FLAGS_APPEND := $(call convert-object-flags,func.o,-DAPPEND)
+LOCAL_OBJECT_FLAGS_OVERRIDE := $(call convert-object-flags,main.o,-DOVERRIDE)
+
 include $(BUILD_EXECUTABLE)
 
